675c6734003eadf4d4c679a8b003230c5e5e5dce
Your Name <jannat27120@gmail.com>
2024-10-26 00:31:42 +0600

    modified blogs.json

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,8 +1,14 @@
 import PropTypes from "prop-types";
 
 const Blog = ({ blog }) => {
-    console.log(blog);
-    return <div></div>;
+    // console.log(blog);
+    const { title, cover } = blog;
+    return (
+        <div className="blog">
+            <img src={cover} alt={`Cover of ${title}`} />
+            <h2 className="text-4xl">{title}</h2>
+        </div>
+    );
 };
 
 Blog.propTypes = {
